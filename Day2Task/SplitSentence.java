aa995408e7121a937b3ad6ea24864f4c586827aa
Siva@28 <2322kb51@kgcas.com>
2025-06-18 12:40:47 +0530

    Add files via upload

---
+++ Day2Task/SplitSentence.java
@@ -0,0 +1,24 @@
+package Day2Task;
+
+
+	import java.util.Scanner;
+
+	public class SplitSentence {
+	    public static void main(String[] args) {
+	        Scanner scanner = new Scanner(System.in);
+
+	        System.out.print("Enter a sentence: ");
+	        String sentence = scanner.nextLine();
+
+	        
+	        String[] words = sentence.split("\\s+");  
+
+	        System.out.println("Words in the sentence:");
+	        for (String word : words) {
+	            System.out.println(word);
+	        }
+
+	        scanner.close();
+	    }
+	}
+
